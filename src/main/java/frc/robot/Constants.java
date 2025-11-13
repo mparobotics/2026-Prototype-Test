51506316bca5f4d8c2aae7e05a00955c2e13b4bb
AlexHuie <ahuie26@moundsparkacademy.org>
2025-11-13 09:01:22 -0600

    Constants Changes
    Fixed the explanation for constants and changed the motor 1 id to 15 to work with the electronics board

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public class Constants {
     public final class MotorConstants{       //for organizaiton 
-        public static final int MOTOR_1_ID = 1; //acts as a constant hence constant ex in math: 14+c and c is a constant 
-        public static final double motorSpeedMultiplier = 1; //constants are a name for a value
+        public static final int MOTOR_1_ID = 15; //acts as a constant value. ex in math: 14+c given that c = 6. c is a constant 
+        public static final double motorSpeedMultiplier = 1; //constants are values that don't change
         //static is used for multiple instances. Static variables are shared and accessable everywhere.  
     }
 }
